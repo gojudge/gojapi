7af5adb458c0a69b4509d0c774020111dfb22786
Rex Lee <duguying2008@gmail.com>
2015-02-12 17:01:37 +0800

    finish login method and add httpclient library

--- src/net/duguying/goj/JudgerHTTP.java
+++ src/net/duguying/goj/JudgerHTTP.java
@@ -1,6 +1,14 @@
 package net.duguying.goj;
 
-import java.util.Map;
+import java.io.IOException;
+import java.util.*;
+
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.methods.PostMethod;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * Created by rex on 2015/1/26.
@@ -25,8 +33,21 @@ public class JudgerHTTP {
      * @param content
      * @return
      */
-    private String Post(String content){
-        return "";
+    private String Post(String content) throws IOException {
+        HttpClient http = new HttpClient();
+        http.getHostConfiguration().setHost(this.host, this.port, "http");
+
+        PostMethod method = new PostMethod();
+        method.setRequestBody(content);
+
+        try {
+            http.executeMethod(method);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "";
+        }
+
+        return method.getResponseBodyAsString();
     }
 
     /**
@@ -35,25 +56,44 @@ public class JudgerHTTP {
      * @return
      */
     private boolean Login(String password){
-        return false;
+        Map<String, Object> reqObj = new HashMap<String, Object>();
+        reqObj.put("password",this.password);
+
+        Map respObj;
+        try {
+            respObj = this.Request(reqObj);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return false;
+        }
+
+        return (boolean)respObj.get("result");
     }
 
     /**
      * pack the message
-     * @param msg the k-v message
+     * @param map the k-v message
      * @return
      */
-    public Map<String, Object> MsgPack(Map<String, Object> msg){
-        return null;
+    public String MsgPack(Map<String, Object> map){
+        JSONObject json = new JSONObject(map);
+        return json.toString();
     }
 
     /**
      * send a request
-     * @param obj the k-v content
+     * @param map the k-v content
      * @return the k-v response
      */
-    public Map<String, Object> Request(Map<String, Object> obj){
-        return null;
+    public Map<String, Object> Request(Map<String, Object> map) throws IOException, JSONException {
+        String req = this.MsgPack(map);
+        String resp = this.Post(req);
+
+        JSONObject json = new JSONObject(resp);
+        return this.toMap(json);
     }
 
     /**
@@ -72,5 +112,43 @@ public class JudgerHTTP {
      */
     public Map<String, Object> GetStatus(Map<String, Object> obj){
         return null;
+    }
+
+    // json to map
+    private static Map toMap(JSONObject object) throws JSONException {
+        Map<String, Object> map = new HashMap<String, Object>();
+
+        Iterator<String> keysItr = object.keys();
+        while(keysItr.hasNext()) {
+            String key = keysItr.next();
+            Object value = object.get(key);
+
+            if(value instanceof JSONArray) {
+                value = toList((JSONArray) value);
+            }
+
+            else if(value instanceof JSONObject) {
+                value = toMap((JSONObject) value);
+            }
+            map.put(key, value);
+        }
+        return map;
+    }
+
+    // json to list
+    private static List toList(JSONArray array) throws JSONException {
+        List<Object> list = new ArrayList<Object>();
+        for(int i = 0; i < array.length(); i++) {
+            Object value = array.get(i);
+            if(value instanceof JSONArray) {
+                value = toList((JSONArray) value);
+            }
+
+            else if(value instanceof JSONObject) {
+                value = toMap((JSONObject) value);
+            }
+            list.add(value);
+        }
+        return list;
     }
 }
