5702dc252dc73a2d94473a999d66383494b1f637
Rex Lee <duguying2008@gmail.com>
2015-02-13 17:02:44 +0800

    add httpclient binary and fixed post method

--- src/net/duguying/goj/JudgerHTTP.java
+++ src/net/duguying/goj/JudgerHTTP.java
@@ -2,9 +2,16 @@ package net.duguying.goj;
 
 import java.io.IOException;
 import java.util.*;
+import java.util.List;
 
-import org.apache.commons.httpclient.HttpClient;
-import org.apache.commons.httpclient.methods.PostMethod;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.entity.EntityBuilder;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.client.methods.RequestBuilder;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -18,46 +25,46 @@ public class JudgerHTTP {
     private int port = 80;
     private String url = "";
     private String password = "";
+    private boolean login = false;
 
-    public JudgerHTTP(String host, int port, String password){
+    public JudgerHTTP(String host, int port, String password) {
         this.host = host;
         this.port = port;
         this.password = password;
 
         this.url = "http://" + host + ":" + port;
 
+        this.login = this.Login(this.password);
+
     }
 
     /**
      * post method for http
+     *
      * @param content
      * @return
      */
     private String Post(String content) throws IOException {
-        HttpClient http = new HttpClient();
-        http.getHostConfiguration().setHost(this.host, this.port, "http");
+        CloseableHttpClient httpclient = HttpClients.createDefault();
 
-        PostMethod method = new PostMethod();
-        method.setRequestBody(content);
+        HttpUriRequest login = RequestBuilder.post().setUri(this.url)
+                .setEntity(EntityBuilder.create().setText(content).build()).build();
+        CloseableHttpResponse response = httpclient.execute(login);
+        HttpEntity entity = response.getEntity();
+        httpclient.close();
 
-        try {
-            http.executeMethod(method);
-        } catch (IOException e) {
-            e.printStackTrace();
-            return "";
-        }
-
-        return method.getResponseBodyAsString();
+        return EntityUtils.toString(entity);
     }
 
     /**
      * login
+     *
      * @param password the password
      * @return
      */
-    private boolean Login(String password){
+    private boolean Login(String password) {
         Map<String, Object> reqObj = new HashMap<String, Object>();
-        reqObj.put("password",this.password);
+        reqObj.put("password", this.password);
 
         Map respObj;
         try {
@@ -70,21 +77,23 @@ public class JudgerHTTP {
             return false;
         }
 
-        return (Boolean)respObj.get("result");
+        return (Boolean) respObj.get("result");
     }
 
     /**
      * pack the message
+     *
      * @param map the k-v message
      * @return
      */
-    public String MsgPack(Map<String, Object> map){
+    public String MsgPack(Map<String, Object> map) {
         JSONObject json = new JSONObject(map);
         return json.toString();
     }
 
     /**
      * send a request
+     *
      * @param map the k-v content
      * @return the k-v response
      */
@@ -98,19 +107,21 @@ public class JudgerHTTP {
 
     /**
      * add task
+     *
      * @param obj the k-v content
      * @return the k-v response
      */
-    public Map<String, Object> AddTask(Map<String, Object> obj){
+    public Map<String, Object> AddTask(Map<String, Object> obj) {
         return null;
     }
 
     /**
      * get task status and result
+     *
      * @param obj the k-v request content
      * @return the k-v response
      */
-    public Map<String, Object> GetStatus(Map<String, Object> obj){
+    public Map<String, Object> GetStatus(Map<String, Object> obj) {
         return null;
     }
 
@@ -119,15 +130,13 @@ public class JudgerHTTP {
         Map<String, Object> map = new HashMap<String, Object>();
 
         Iterator<String> keysItr = object.keys();
-        while(keysItr.hasNext()) {
+        while (keysItr.hasNext()) {
             String key = keysItr.next();
             Object value = object.get(key);
 
-            if(value instanceof JSONArray) {
+            if (value instanceof JSONArray) {
                 value = toList((JSONArray) value);
-            }
-
-            else if(value instanceof JSONObject) {
+            } else if (value instanceof JSONObject) {
                 value = toMap((JSONObject) value);
             }
             map.put(key, value);
@@ -138,13 +147,11 @@ public class JudgerHTTP {
     // json to list
     private static List toList(JSONArray array) throws JSONException {
         List<Object> list = new ArrayList<Object>();
-        for(int i = 0; i < array.length(); i++) {
+        for (int i = 0; i < array.length(); i++) {
             Object value = array.get(i);
-            if(value instanceof JSONArray) {
+            if (value instanceof JSONArray) {
                 value = toList((JSONArray) value);
-            }
-
-            else if(value instanceof JSONObject) {
+            } else if (value instanceof JSONObject) {
                 value = toMap((JSONObject) value);
             }
             list.add(value);
--- test/net/duguying/goj/JudgerTCPTest.java
+++ test/net/duguying/goj/JudgerTCPTest.java
@@ -5,19 +5,24 @@ import java.io.IOException;
 
 public class JudgerTCPTest extends TestCase {
     private JudgerTCP j = null;
+    private JudgerHTTP h = null;
 
-    public void testJudgerTCP() {
-        try {
-            this.j = new JudgerTCP("oj.duguying.net", 1004, "123456789");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+//    public void testJudgerTCP() {
+//        try {
+//            this.j = new JudgerTCP("oj.duguying.net", 1004, "123456789");
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
+//        // assert connect success
+//        assertNotNull(this.j);
+//
+//        this.j = null;
+//        System.gc();
+//    }
 
-        // assert connect success
-        assertNotNull(this.j);
-
-        this.j = null;
-        System.gc();
+    public void testJudgerHTTP(){
+        this.h = new JudgerHTTP("127.0.0.1", 1005, "123456789");
     }
 
 }
