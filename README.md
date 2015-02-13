c84c9a0f292d5e3d86b976b9ab29829e906c1556
Rex Lee <duguying2008@gmail.com>
2015-02-13 23:28:28 +0800

    change project as maven project and add travis ci

---
+++ README.md
@@ -0,0 +1,13 @@
+# goj judger api for java
+
+here is the goj judger api for java developer.
+
+## Build
+
+```shell
+mvn package
+```
+
+## License
+
+MIT License
--- src/test/java/net/duguying/goj/JudgerHTTPTest.java
+++ src/test/java/net/duguying/goj/JudgerHTTPTest.java
@@ -6,10 +6,12 @@ import junit.framework.TestCase;
  * Created by rex on 2015/1/26.
  */
 public class JudgerHTTPTest extends TestCase {
+	private JudgerHTTP h = null;
+	
     protected void setUp() {
     }
-
-    public void testJudgerHTTP() {
-
+	
+	public void testJudgerHTTP(){
+        this.h = new JudgerHTTP("oj.duguying.net", 1005, "123456789");
     }
 }
